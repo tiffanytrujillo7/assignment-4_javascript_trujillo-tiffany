b09fde1332d22a10b65749befd22f12394730fcb
Tiffany Trujillo <tiffanytrujillo@Tiffanys-MacBook-Air.local>
2018-06-14 15:58:54 -0400

    Made changes to presidents console log.

--- js/main.js
+++ js/main.js
@@ -10,40 +10,41 @@ console.log(iceCream);
 
 //Presidents Variable
 
-var presidents = {
-     {
-        firstName: "Donald",
+var presidents = [
+      { firstName: "Donald",
         lastName: "Trump",
         termLength: 1,
         party: "Republican",
-        yearsOfPresidency: "2017-2021";
+        yearsOfPresidency: "2017-2021"
       },
       {
         firstName: "Barrack",
         lastName: "Obama",
         termLength: 8,
         party: "Democrat",
-        yearsOfPresidency: "2009-2017";
+        yearsOfPresidency: "2009-2017"
       },
       {
         firstName: "George W.",
         lastName: "Bush",
         termLength: 8,
         party: "Republican",
-        yearsOfPresidency: "2001-2009";
+        yearsOfPresidency: "2001-2009"
       },
       {
         firstName: "Bill",
         lastName: "Clinton",
         termLength: 8,
         party: "Democrat",
-        yearsOfPresidency: "1993-2001";
+        yearsOfPresidency: "1993-2001"
       },
       {
         firstName: "George H. W.",
         lastName: "Bush",
         termLength: 4,
         party: "Republican",
-        yearsOfPresidency: "1989-1993";
-      },
-    }
+        yearsOfPresidency: "1989-1993"
+      }
+    ];
+
+console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
